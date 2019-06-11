75f548135e170f6094969c01fc4cfae0eea4d404
vieira-andre <andre.vieira@outlook.com>
2019-06-11 04:01:32 -0300

    Add size property and initialize it on the constructor

--- AbstractSorting.java
+++ AbstractSorting.java
@@ -1,7 +1,9 @@
 abstract class AbstractSorting implements ISortable {   
     int[] arr;
+    int size;
 
     public AbstractSorting(int[] arr) {
         this.arr = arr;
+        this.size = arr.length;
     }
 }
