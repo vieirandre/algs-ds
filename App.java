0db4b23d3114d0997c3a27731bcb2f389b9ee85d
vieira-andre <andre.vieira@outlook.com>
2019-06-11 04:14:20 -0300

    Extract output printing to method

--- App.java
+++ App.java
@@ -1,10 +1,14 @@
 public class App {
     public static void main(String[] args) {
-        int[] input = new int[] {5, 7, 3, 1, 4, 2, 6};
+        int[] input = new int[] { 5, 7, 3, 1, 4, 2, 6 };
 
         SelectionSort algorithm = new SelectionSort(input);
         algorithm.sort();
 
+        printOutput(input);
+    }
+
+    private static void printOutput(int[] input) {
         for (int e : input) {
             System.out.print(e + " ");
         }
