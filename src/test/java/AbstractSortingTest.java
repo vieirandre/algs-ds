063396ca88e6dbdf31333c041c8b8a47023d9bca
vieira-andre <andre.vieira@outlook.com>
2019-06-23 06:34:02 -0300

    Change initialize method annotation to @BeforeClass

--- src/test/java/AbstractSortingTest.java
+++ src/test/java/AbstractSortingTest.java
@@ -1,14 +1,14 @@
 import static org.junit.Assert.assertArrayEquals;
 
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class AbstractSortingTest {
-    int[] input, expected;
+    static int[] input, expected;
     AbstractSorting algorithm;
     
-    @Before
-    public void initialize() {
+    @BeforeClass
+    public static void initialize() {
         input = new int[] { 5, 7, 3, 1, 4, 2, 6 };
         expected = new int[] { 1, 2, 3, 4, 5, 6, 7 };
     }
