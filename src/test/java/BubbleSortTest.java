e99c345460fa318749881f6d3ce776294e91ecdc
vieira-andre <andre.vieira@outlook.com>
2019-06-23 06:19:31 -0300

    Refactor the BubbleSortTest class

--- src/test/java/BubbleSortTest.java
+++ src/test/java/BubbleSortTest.java
@@ -5,17 +5,17 @@ import org.junit.Test;
 
 public class BubbleSortTest {
     int[] input, expected;
+    BubbleSort algorithm;
     
     @Before
     public void initialize() {
         input = new int[] { 5, 7, 3, 1, 4, 2, 6 };
+        expected = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+        algorithm = new BubbleSort(input);
     }
 
     @Test()
-    public void testIt() {
-        expected = new int[] { 1, 2, 3, 4, 5, 6, 7 };
-
-        BubbleSort algorithm = new BubbleSort(input);
+    public void testSimpleSort() {
         algorithm.sort();
 
         assertArrayEquals(expected, algorithm.arr);
