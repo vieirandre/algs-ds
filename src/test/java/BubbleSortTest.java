f26347c193ab54775f4fe00c779010d7bc385300
vieira-andre <andre.vieira@outlook.com>
2019-06-25 20:59:01 -0300

    Create test class for BubbleSort

---
+++ src/test/java/BubbleSortTest.java
@@ -0,0 +1,51 @@
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class BubbleSortTest {
+    AbstractSorting algorithm;
+    static AbstractSortingTestHelper helper;
+
+    @BeforeClass
+    public static void initialize() {
+        helper = new AbstractSortingTestHelper();
+
+        helper.initialize_SimpleArray();
+        helper.initialize_OrderedArray();
+        helper.initialize_ReversedArray();
+        helper.initialize_LargeArray();
+    }
+
+    @Test
+    public void testSimpleInput_BubbleSort() {
+        algorithm = new BubbleSort(helper.getSimpleInput());
+        algorithm.sort();
+
+        assertArrayEquals(helper.getOutputForSimpleInput(), algorithm.arr);
+    }
+
+    @Test
+    public void testLargeInput_BubbleSort() {
+        algorithm = new BubbleSort(helper.getLargeInput());
+        algorithm.sort();
+
+        assertArrayEquals(helper.getOutputForLargeInput(), algorithm.arr);
+    }
+
+    @Test
+    public void testOrderedInput_BubbleSort() {
+        algorithm = new BubbleSort(helper.getOrderedInput());
+        algorithm.sort();
+
+        assertArrayEquals(helper.getOutputForOrderedInput(), algorithm.arr);
+    }
+
+    @Test
+    public void testReversedInput_BubbleSort() {
+        algorithm = new BubbleSort(helper.getReversedInput());
+        algorithm.sort();
+
+        assertArrayEquals(helper.getOutputForReversedInput(), algorithm.arr);
+    }
+}
