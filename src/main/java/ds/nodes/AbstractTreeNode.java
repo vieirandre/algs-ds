317da53af53d6843d61180107150045a23d88399
vieira-andre <andre.vieira@outlook.com>
2023-02-12 08:14:35 -0300

    Implement abstract tree node

---
+++ src/main/java/ds/nodes/AbstractTreeNode.java
@@ -0,0 +1,18 @@
+package ds.nodes;
+
+abstract class AbstractTreeNode<T> {
+    protected T value;
+    protected AbstractTreeNode<T> parent;
+
+    public AbstractTreeNode(T value) {
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+}
