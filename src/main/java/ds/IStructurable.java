36654fb3229dc99e4d61a76ee92c82c2ef36e8ac
vieira-andre <andre.vieira@outlook.com>
2023-09-18 02:23:04 -0300

    Move `insert` and `remove` methods to `ITree`

--- src/main/java/ds/IStructurable.java
+++ src/main/java/ds/IStructurable.java
@@ -1,10 +1,6 @@
 package ds;
 
 public interface IStructurable<T> {
-  void insert(T item);
-
-  T remove(T item);
-
   boolean contains(T item);
 
   boolean isEmpty();
--- src/main/java/ds/ITree.java
+++ src/main/java/ds/ITree.java
@@ -3,6 +3,10 @@ package ds;
 import ds.nodes.AbstractTreeNode;
 
 public interface ITree<T, N extends AbstractTreeNode<T, N>> extends IStructurable<T> {
+    void insert(T item);
+
+    T remove(T item);
+
     N getRoot();
 
     N getParent(N node);
